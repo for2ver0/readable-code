66061ed694a68aaf38a9f0896f8c74557aeeba03
eun0 <shinely12@gmail.com>
2025-06-06 22:31:48 +0900

    [섹션 7. 리팩토링 연습] 46. 리팩토링 (1) - 추상화 레벨 (10)
    [추상화] 후보를 추출하는 로직을 메서드로 추상화

--- src/main/java/cleancode/studycafe/tobe/StudyCafePassMachine.java
+++ src/main/java/cleancode/studycafe/tobe/StudyCafePassMachine.java
@@ -35,15 +35,20 @@ public class StudyCafePassMachine {
 
     private StudyCafePass selectPass() {
         outputHandler.askPassTypeSelection();
-        StudyCafePassType studyCafePassType = inputHandler.getPassTypeSelectingUserAction();
+        StudyCafePassType passType = inputHandler.getPassTypeSelectingUserAction();
 
-        List<StudyCafePass> studyCafePasses = studyCafeFileHandler.readStudyCafePasses();
-        List<StudyCafePass> passCandidates = studyCafePasses.stream()
-                .filter(studyCafePass -> studyCafePass.getPassType() == studyCafePassType)
-                .toList();
+        List<StudyCafePass> passCandidates = findPassCandidatesBy(passType);
 
         outputHandler.showPassListForSelection(passCandidates);
         return inputHandler.getSelectPass(passCandidates);
+    }
+
+    private List<StudyCafePass> findPassCandidatesBy(StudyCafePassType studyCafePassType) {
+        List<StudyCafePass> allPasses = studyCafeFileHandler.readStudyCafePasses();
+
+        return allPasses.stream()
+                .filter(studyCafePass -> studyCafePass.getPassType() == studyCafePassType)
+                .toList();
     }
 
     private StudyCafeLockerPass selectLockerPass(StudyCafePass selectedPass) {
