08e11cb837286f16e25566faf93c02a71258a43e
eun0 <shinely12@gmail.com>
2025-06-06 18:21:33 +0900

    [섹션 7. 리팩토링 연습] 46. 리팩토링 (1) - 추상화 레벨 (6)
    [중복 제거] FileHandler를 공통 필드로

--- src/main/java/cleancode/studycafe/tobe/StudyCafePassMachine.java
+++ src/main/java/cleancode/studycafe/tobe/StudyCafePassMachine.java
@@ -14,6 +14,7 @@ public class StudyCafePassMachine {
 
     private final InputHandler inputHandler = new InputHandler();
     private final OutputHandler outputHandler = new OutputHandler();
+    private final StudyCafeFileHandler studyCafeFileHandler = new StudyCafeFileHandler();
 
     public void run() {
         try {
@@ -23,7 +24,6 @@ public class StudyCafePassMachine {
             outputHandler.askPassTypeSelection();
             StudyCafePassType studyCafePassType = inputHandler.getPassTypeSelectingUserAction();
 
-            StudyCafeFileHandler studyCafeFileHandler = new StudyCafeFileHandler();
             List<StudyCafePass> studyCafePasses = studyCafeFileHandler.readStudyCafePasses();
             List<StudyCafePass> passCandidates = studyCafePasses.stream()
                     .filter(studyCafePass -> studyCafePass.getPassType() == studyCafePassType)
@@ -37,7 +37,7 @@ public class StudyCafePassMachine {
             } else if (studyCafePassType == StudyCafePassType.WEEKLY) {
                 outputHandler.showPassOrderSummary(selectedPass, null);
             } else if (studyCafePassType == StudyCafePassType.FIXED) {
-                StudyCafeLockerPass lockerPass = selectLockerPass(studyCafeFileHandler, selectedPass);
+                StudyCafeLockerPass lockerPass = selectLockerPass(selectedPass);
 
                 boolean lockerSelection = false;
                 if (lockerPass != null) {
@@ -58,7 +58,7 @@ public class StudyCafePassMachine {
         }
     }
 
-    private StudyCafeLockerPass selectLockerPass(StudyCafeFileHandler studyCafeFileHandler, StudyCafePass selectedPass) {
+    private StudyCafeLockerPass selectLockerPass(StudyCafePass selectedPass) {
         List<StudyCafeLockerPass> lockerPasses = studyCafeFileHandler.readLockerPasses();
 
         return lockerPasses.stream()
